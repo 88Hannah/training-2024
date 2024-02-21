54b395c1802893dea660f4b016597099e81ca4a2
Hannah Sangar <hannahsangar@gmail.com>
2024-02-21 16:43:58 +0200

    Data added to page and styled from course files

--- van-life/src/pages/VanDetail.jsx
+++ van-life/src/pages/VanDetail.jsx
@@ -1,9 +1,34 @@
-
+import React from 'react'
 import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
 
+    const [van, setVan] = React.useState()
+
+
     const vanId = (useParams().id)
 
-    return <h1>Van details for van number {vanId}</h1>
+    React.useEffect(() => {
+        fetch(`/api/vans/${vanId}`)
+            .then(res => res.json())
+            .then(data => {
+                setVan(data.vans)
+            })
+    }, [vanId])
+
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    )
 }
