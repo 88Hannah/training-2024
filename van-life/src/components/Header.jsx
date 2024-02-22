b5f6918f79244e08721cd99beaaa657c249c1c51
Hannah Sangar <hannahsangar@gmail.com>
2024-02-22 12:06:54 +0200

    NavLinks styles - desctucture the object to acces the isActive property

--- van-life/src/components/Header.jsx
+++ van-life/src/components/Header.jsx
@@ -1,15 +1,40 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 export default function Header() {
-  return (
-    <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-            <Link to='/host'>Host</Link>
-            <Link to="/about">About</Link>
-            <Link to='/vans'>Vans</Link>
-        </nav>
-    </header>
-  )
+
+    const activeStyles = {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        color: '#161616'
+    }
+
+    return (
+        <header>
+            <NavLink className="site-logo" to="/">#VanLife</NavLink>
+            <nav>
+                <NavLink 
+                    to='/host'
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+
+                <NavLink 
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+
+                <NavLink 
+                    to='/vans'
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+
+            </nav>
+        </header>
+    )
 }
