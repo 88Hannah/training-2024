896e10f8fec685d1955c4ae8d4ac05f22b679ceb
Hannah Sangar <hannahsangar@gmail.com>
2024-02-27 10:39:50 +0200

    Login page implemented and re routes to prev page once logged in

--- van-life/src/components/AuthRequired.jsx
+++ van-life/src/components/AuthRequired.jsx
@@ -1,13 +1,18 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
 export default function AuthRequired() {
 
-    const authenticate = false;
+    const isLoggedIn = localStorage.getItem("loggedin")
+    let location = useLocation()
 
-    if (!authenticate) {
+    if (!isLoggedIn) {
         return <Navigate 
             to='/login'
-            state={{message: "You must log in first"}}    
+            state={{
+                message: "You must log in first",
+                prevPath: location.pathname
+            }}
+            replace={true}  
         />
     }
 
